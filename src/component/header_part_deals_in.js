2089c342be624877d9d55528e0c920bed7267f8d
sahiltapu <sahilmishranayagarh@gmail.com>
2023-11-02 16:58:51 +0530

    fiest commit

---
+++ src/component/header_part_deals_in.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./header_part_deals_in.css"
+
+
+const Header_part_deals_in = () =>{
+    return(
+        <>
+        <div className="Header_part_deals_in_outer_div">
+            <div className="Deals_in_div">
+                <p style={{textAlign:"left"}}>
+                    <strong><i>DEALS IN - </i></strong>Construction Equipments and Government order supplier.<br /><br />
+                        <strong className="GSTN">GSTN - 21BEGPM6426K1ZH</strong>
+
+                </p>
+            </div>
+        </div>
+        
+        
+        </>
+    )
+
+}
+
+export default Header_part_deals_in;
